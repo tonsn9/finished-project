80fd03a96d05e9de1bbc1c0b1d2918b14fbf7df8
Ton Soares <ton.sn9@gmail.com>
2022-07-10 20:41:41 -0300

    feat: finished-project

--- Exercícios/exercicio03.js
+++ Exercícios/exercicio03.js
@@ -1,4 +1,4 @@
-// Exercicio 03 
+// Exercicio 03
 // Faça uma chamada para a api "rick and morty" e resgate informações dos seguintes personagens: Rick
 // Sanchez, Morty Smith, Summer Smith, Beth Smith, Jerry Smith. Ajuste os dados para que fiquem igual à
 // saída de exemplo abaixo.
@@ -6,6 +6,17 @@
 // https://rickandmortyapi.com/documentation/#rest
 
 async function getRickAndMortyCharacters() {
-    
+  const response = await fetch(
+    "https://rickandmortyapi.com/api/character/1,2,3,4,5"
+  );
+  const personagens = await response.json();
+  return personagens.map((item) => {
+    return {
+      nome: item.name,
+      genero: item.gender,
+      avatar: item.image,
+      especie: item.species,
+    };
+  });
 }
-    export default getRickAndMortyCharacters;
+module.export = getRickAndMortyCharacters;
